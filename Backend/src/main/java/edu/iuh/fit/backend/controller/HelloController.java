9e65edefcc0f2bf61976cc41f745e2de6a72605a
HuuThai <levanhuuthai2004@gmail.com>
2025-08-12 15:11:24 +0700

    first commit

---
+++ Backend/src/main/java/edu/iuh/fit/backend/controller/HelloController.java
@@ -0,0 +1,22 @@
+/*
+ * @ (#) .java    1.0       
+ * Copyright (c)  IUH. All rights reserved.
+ */
+package edu.iuh.fit.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * @description
+ * @author: Huu Thai
+ * @date:   
+ * @version: 1.0
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String getHello(){
+        return "Hello spring boot with alo";
+    }
+}
