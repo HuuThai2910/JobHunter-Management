78ededd74ae415e0429f4ebaeb8e0a4fd26e2755
HuuThai <levanhuuthai2004@gmail.com>
2025-09-16 15:30:18 +0700

    Interceptor permission with backend

---
+++ Backend/src/main/java/edu/iuh/fit/backend/config/PermissionInterceptor.java
@@ -0,0 +1,73 @@
+/*
+ * @ (#) .java    1.0
+ * Copyright (c)  IUH. All rights reserved.
+ */
+package edu.iuh.fit.backend.config;
+
+/*
+ * @description
+ * @author: Huu Thai
+ * @date:
+ * @version: 1.0
+ */
+
+import edu.iuh.fit.backend.domain.Permission;
+import edu.iuh.fit.backend.domain.Role;
+import edu.iuh.fit.backend.domain.User;
+import edu.iuh.fit.backend.service.UserService;
+import edu.iuh.fit.backend.util.SecurityUtil;
+import edu.iuh.fit.backend.util.error.InvalidException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+
+import java.util.List;
+
+
+public class PermissionInterceptor implements HandlerInterceptor {
+    @Autowired
+    UserService userService;
+
+    @Transactional
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response, Object handler)
+            throws Exception {
+
+        String path = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String requestURI = request.getRequestURI();
+        String httpMethod = request.getMethod();
+        System.out.println(">>> RUN preHandle");
+        System.out.println(">>> path= " + path);
+        System.out.println(">>> httpMethod= " + httpMethod);
+        System.out.println(">>> requestURI= " + requestURI);
+
+//        Check permission
+        String email = SecurityUtil.getCurrentUserLogin().isPresent()
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+        if (!email.isEmpty()) {
+            User user = this.userService.getUserByUserName(email);
+            if (user != null) {
+                Role role = user.getRole();
+                if (role != null) {
+                    List<Permission> permissions = role.getPermissions();
+                    boolean isAllow = permissions.stream().anyMatch(permission ->
+                            permission.getApiPath().equals(path) && permission.getMethod().equals(httpMethod)
+                    );
+                    if(!isAllow){
+                        throw new InvalidException("You do not have permission to access this endpoint");
+                    }
+                }
+            }else {
+                throw new InvalidException("You do not have permission to access this endpoint");
+            }
+        }
+        return true;
+    }
+}
+
---
+++ Backend/src/main/java/edu/iuh/fit/backend/config/PermissionInterceptorConfiguration.java
@@ -0,0 +1,36 @@
+/*
+ * @ (#) .java    1.0
+ * Copyright (c)  IUH. All rights reserved.
+ */
+package edu.iuh.fit.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+ * @description
+ * @author: Huu Thai
+ * @date:
+ * @version: 1.0
+ */
+//Duoc su dung khi nguoi dung truy cap vao mot duong link khong duoc cap quyen
+//No hoat dong sau khi spring security chay va truoc khi request duoc dua toi controller
+@Configuration
+public class PermissionInterceptorConfiguration implements WebMvcConfigurer {
+    @Bean
+    PermissionInterceptor getPermissionInterceptor() {
+        return new PermissionInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        String[] whiteList = {
+                "/", "/api/v1/auth/**", "/storage/**",
+                "/api/v1/companies", "/api/v1/jobs", "/api/v1/skills", "/api/v1/files"
+        };
+        registry.addInterceptor(getPermissionInterceptor())
+                .excludePathPatterns(whiteList);
+    }
+}
